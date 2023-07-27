0939c721ad591d2be72bb3e45246e60bccbd7eb6
G-VAISHNAVI-2003 <139878536+G-VAISHNAVI-2003@users.noreply.github.com>
2023-07-27 19:49:18 +0530

    Pairsum commit

---
+++ pairsum.py
@@ -0,0 +1,13 @@
+def pairSum():
+    s = int(input())
+    arr= list(map(int,input().split()))
+
+    n = len(arr)
+    for i in range(0,n):
+        for j in range(i+1,n):
+            if arr[i]+arr[j] == s:
+                print(arr[i], arr[j])
+
+    return -1
+pairSum()
+5
